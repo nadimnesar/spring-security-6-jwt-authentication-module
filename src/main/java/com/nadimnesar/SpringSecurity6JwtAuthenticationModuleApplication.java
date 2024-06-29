a34bc232b15cc2c00f055487ea5bca0626cfd21a
nadimnesar <nadimnesar.cse@gmail.com>
2024-06-30 00:24:56 +0600

    project setup

---
+++ src/main/java/com/nadimnesar/SpringSecurity6JwtAuthenticationModuleApplication.java
@@ -0,0 +1,11 @@
+package com.nadimnesar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity6JwtAuthenticationModuleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity6JwtAuthenticationModuleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nadimnesar/SpringSecurity6JwtAuthenticationModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadimnesar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity6JwtAuthenticationModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
